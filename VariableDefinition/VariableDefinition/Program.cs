44958e001db0cec1355f46bd26a5cf350e88163f
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-10 15:26:07 +0800

    VariableDefinition

---
+++ VariableDefinition/VariableDefinition/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VariableDefinition {
+    class Program {
+        static void Main(string[] args) {
+            short a;
+            int b;
+            double c;
+
+            a = 10;
+            b = 20;
+            c = a + b;
+            System.Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            System.Console.ReadLine();
+        }
+    }
+}
