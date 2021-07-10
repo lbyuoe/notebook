e400944078be3bd23da4a2ad9cb5dc629d450c44
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-10 15:22:51 +0800

    TypeConversion

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TypeConversionApplication {
+    class ExplicitConversion {
+        static void Main(string[] args) {
+            double d = 5673.74;
+            int i;
+
+            i = (int)d;
+            System.Console.WriteLine(i); ;
+            System.Console.ReadKey();
+        }
+    }
+}
