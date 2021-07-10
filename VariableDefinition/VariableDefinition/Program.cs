20dea24066a67a5a9481b59d886e770495ae851b
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-10 15:31:18 +0800

    VariableDefinition update

--- VariableDefinition/VariableDefinition/Program.cs
+++ VariableDefinition/VariableDefinition/Program.cs
@@ -9,7 +9,10 @@
             b = 20;
             c = a + b;
             System.Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
-            System.Console.ReadLine();
+
+            int num;
+            num = System.Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("{0}", num);
         }
     }
 }
