e5bd833701e52e889ceaacfb0dd0859f37a1272a
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-12 15:49:14 +0800

    FileIO upload

---
+++ C#/FileIO/FileIO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO {
+    class Program {
+        static void Main(string[] args) {
+            FileStream F = new FileStream("test.txt", FileMode.Open, FileAccess.ReadWrite);
+
+            for (int i = 1; i <= 20; i++) {
+                F.WriteByte((byte)i);
+            }
+            F.Position = 0;
+            for (int i = 0; i <= 20; i++) {
+                Console.WriteLine(F.ReadByte() + "");
+            }
+            F.Close();
+            Console.ReadKey();
+        }
+    }
+}
