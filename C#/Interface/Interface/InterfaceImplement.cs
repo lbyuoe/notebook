306412576e1ffb10743727cd0d472837253658f1
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-12 08:32:37 +0800

    Interface upload

---
+++ C#/Interface/Interface/InterfaceImplement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class InterfaceImplementer : IMyInterface {
+    public void MethodToImplement() {
+        Console.WriteLine("MethodToImplement() called.");
+    }
+
+    static void Main() {
+        InterfaceImplementer iImp = new InterfaceImplementer();
+        iImp.MethodToImplement();
+    }
+}
---
+++ C#/Interface/Interface/MyInterface.cs
@@ -0,0 +1,3 @@
+﻿interface IMyInterface {
+    void MethodToImplement();
+}
