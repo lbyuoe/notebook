054d02ce9b09dc440f73ff3f91d4349c2288addf
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-12 09:06:55 +0800

    InterfaceNest upload

---
+++ C#/InterfaceNest/InterfaceNest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SomeNameSpace {
+    namespace Nested {
+        public class NestdNameSpaceClass {
+            public static void SayHello() {
+                Console.WriteLine("In Nested");
+            }
+        }
+    }
+
+    public class MyClass {
+        static void Main() {
+            Console.WriteLine("In SomeNameSpace");
+            Nested.NestdNameSpaceClass.SayHello();
+        }
+    }
+}
