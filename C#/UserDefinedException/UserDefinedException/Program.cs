2c6d9a687ff3dfc334120cad69d27e20f89ac605
Isaac <39878177+lbyuoe@users.noreply.github.com>
2021-07-12 15:33:12 +0800

    UserDefinedException upload

---
+++ C#/UserDefinedException/UserDefinedException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserDefinedException {
+    class TestTemperature {
+        static void Main(string[] args) {
+            Temperature temp = new Temperature();
+            try {
+                temp.showTemp();
+            } catch (TempIsZeroException e) {
+                Console.WriteLine("TempIsZereException: {0}", e.Message);
+            }
+            Console.ReadKey();
+         }
+    }
+}
+
+public class TempIsZeroException : ApplicationException {
+    public TempIsZeroException(string message) : base(message) { }
+}
+
+public class Temperature {
+    int temperature = 0;
+    public void showTemp() {
+        if (temperature == 0) {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        } else {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+}
